914c3e3b095f9bdc8439ff89d75e70e5c56b7e98
Nikita G <grishko188@gmail.com>
2018-03-23 11:44:18 +0200

    Initial commit

---
+++ app/src/main/java/com/nullgr/androidcore/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nullgr.androidcore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var view = View(this)
+    }
+}
