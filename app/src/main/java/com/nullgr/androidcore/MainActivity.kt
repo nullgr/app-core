a73e03433bfa23134b7a7191522bab32be202f51
vchernyshov <morkovkacorp@gmail.com>
2018-04-04 11:25:14 +0300

    [AC-2] Made params at use cases not required

--- app/src/main/java/com/nullgr/androidcore/MainActivity.kt
+++ app/src/main/java/com/nullgr/androidcore/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.nullgr.androidcore
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
--- corelibrary/src/main/java/com/nullgr/corelibrary/interactor/CompletableUseCase.kt
+++ corelibrary/src/main/java/com/nullgr/corelibrary/interactor/CompletableUseCase.kt
@@ -7,18 +7,18 @@ import io.reactivex.Completable
 /**
  * Abstract class for a UseCase that returns an instance of a [Completable].
  */
-abstract class CompletableUseCase<T, in Params> protected constructor(
-        val schedulersFacade: SchedulersFacade) {
+abstract class CompletableUseCase<in Params> protected constructor(
+        private val schedulersFacade: SchedulersFacade) {
 
     /**
      * Builds a [Completable] which will be used when the current [CompletableUseCase] is executed.
      */
-    protected abstract fun buildUseCaseObservable(params: Params): Completable
+    protected abstract fun buildUseCaseObservable(params: Params? = null): Completable
 
     /**
      * Executes the current use case.
      */
-    open fun execute(params: Params): Completable {
+    open fun execute(params: Params? = null): Completable {
         return buildUseCaseObservable(params).applySchedulers(schedulersFacade)
     }
 }
