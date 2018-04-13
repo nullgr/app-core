e67ad6bef7ded5249a5c37c42f5490c3f78f0d61
Nikita G <grishko188@gmail.com>
2018-04-13 12:46:23 +0300

    - Refactor ScreenExtensions - Add KDoc to ScreenExtensions and  ResourceProvider

--- corelibrary/src/main/java/com/nullgr/corelibrary/resources/ScreenExtensions.kt
+++ corelibrary/src/main/java/com/nullgr/corelibrary/resources/ScreenExtensions.kt
@@ -6,32 +6,76 @@ import android.graphics.Point
 import android.view.WindowManager
 
 /**
- * Created by Grishko Nikita on 01.02.18.
+ * Convert physical pixels to dpi
+ * @receiver [Float] value of physical pixels
+ * @param resources instance of [Resources]
+ * @return [Float] value in dpi
  */
-fun px2dp(resources: Resources, px: Float): Float {
-    return px / resources.displayMetrics.density
+fun Float.pxTodp(resources: Resources): Float {
+    return this / resources.displayMetrics.density
 }
 
-fun dp2px(resources: Resources, dp: Float): Float {
-    return dp * resources.displayMetrics.density
+/**
+ * Convert dpi to physical pixels
+ * @receiver [Float] value of dpi
+ * @param resources instance of [Resources]
+ * @return [Float] value in physical pixels
+ */
+fun Float.dpTopx(resources: Resources): Float {
+    return this * resources.displayMetrics.density
 }
 
-fun spToPx(resources: Resources, sp: Float): Float {
+/**
+ * Convert physical pixels to dpi
+ * @receiver [Float] value of physical pixels
+ * @param context instance of [Context]
+ * @return [Float] value in dpi
+ */
+fun Float.pxTodp(context: Context): Float {
+    return this / context.resources.displayMetrics.density
+}
+
+/**
+ * Convert dpi to physical pixels
+ * @receiver [Float] value of dpi
+ * @param context instance of [Context]
+ * @return [Float] value in physical pixels
+ */
+fun Float.dpTopx(context: Context): Float {
+    return this * context.resources.displayMetrics.density
+}
+
+/**
+ * Convert sp to physical pixels
+ * @receiver [Float] value of sp
+ * @param resources instance of [Resources]
+ * @return [Float] value in physical pixels
+ */
+fun Float.spToPx(resources: Resources): Float {
     val scale = resources.displayMetrics.scaledDensity
-    return sp * scale
+    return this * scale
 }
 
+/**
+ * Convert sp to physical pixels
+ * @receiver [Float] value of sp
+ * @param context instance of [Context]
+ * @return [Float] value in physical pixels
+ */
+fun Float.spToPx(context: Context): Float {
+    val scale = context.resources.displayMetrics.scaledDensity
+    return this * scale
+}
+
+/**
+ * Gets the size of the display, in pixels.
+ * @param context instance of [Context]
+ * @return [Pair] object where [Pair.first] is **widthPixels** and [Pair.second] is **heightPixels**
+ * @see [android.view.Display.getSize]
+ */
 fun getDisplaySize(context: Context): Pair<Int, Int> {
     val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     val size = Point()
     windowManager.defaultDisplay.getSize(size)
-    return Pair(size.x, size.y)
-}
-
-fun ResourceProvider.px2dp(px: Float): Float {
-    return px / getDensity()
-}
-
-fun ResourceProvider.dp2px(dp: Float): Float {
-    return dp * getDensity()
+    return size.x to size.y
 }
