e623dfd7f192a6d754604a3534a0a36234a359ee
Nikita G <grishko188@gmail.com>
2018-05-07 17:21:08 +0300

    - Update documentation for RxBus

--- corelibrary/src/main/java/com/nullgr/corelibrary/rx/RxBus.kt
+++ corelibrary/src/main/java/com/nullgr/corelibrary/rx/RxBus.kt
@@ -2,14 +2,23 @@ package com.nullgr.corelibrary.rx
 
 import com.jakewharton.rxrelay2.PublishRelay
 import com.jakewharton.rxrelay2.Relay
+import com.nullgr.corelibrary.rx.RxBus.KEYS
+import com.nullgr.corelibrary.rx.RxBus.KEYS.GENERAL
+import com.nullgr.corelibrary.rx.RxBus.KEYS.SINGLE
 import com.nullgr.corelibrary.rx.relay.SingleSubscriberRelay
 import io.reactivex.Observable
 
 /**
  * Simple ***EVENT BUS*** built in a reactive manner. Provides publish-subscribe-style communication between components.
  * Can be used in two ways: with key or not. For each new key new [Relay] will be created.
- * Without key all events will be posted in in general [Relay]. Usage of this class is thread safe.
- * Use key based relays to split logic in your application. For correct work of bus, you need to post events and subscribe
+ * Without key all events will be posted in in general [Relay]. The same behaviour if use [KEYS.GENERAL].
+ * Usage of this class is thread safe.
+ *
+ * Use key based relays to split logic in your application.
+ * Or you can use keys specified in [KEYS] to access some specific relays, provided with this class.
+ * To get more information about this relays read documentation of [KEYS]
+ *
+ * For correct work of bus, you need to post events and subscribe
  * to receive them, on the same instance of [RxBus]. The best usage is ***Singleton*** pattern.
  * You can implement singleton bus provider by yourself, or use our [SingletonRxBusProvider]
  *
@@ -21,6 +30,10 @@ import io.reactivex.Observable
  * ```
  * SingletonRxBusProvider.BUS.post(TypeOfYourEvent::class.java, SomeEventClass()) // you can use any type/class/object for event type
  * ```
+ * or
+ * ```
+ * SingletonRxBusProvider.BUS.post(RxBus.KEYS.SINGLE, SomeEventClass()) // to use SingleSubscriberRelay as events bus
+ * ```
  * <h2>Receiving Events:</h2>
  * ```
  * SingletonRxBusProvider.BUS.observable()
@@ -31,7 +44,7 @@ import io.reactivex.Observable
  * SingletonRxBusProvider.BUS.observable(TypeOfYourEvent::class.java)
  *          .subscribe{// your code here}
  * ```
- * @author chernyshov.
+ * @author chernyshov, Grishko Nikita
  */
 class RxBus {
 
@@ -47,7 +60,7 @@ class RxBus {
      *
      * @param event [Any] event to post.
      * @param key key to get specific [Relay] in which event will be posted.
-     * If null will be passed - event will be posted in general relay
+     * Default key is [KEYS.GENERAL].
      */
     @JvmOverloads
     fun post(key: Any = KEYS.GENERAL, event: Any) {
@@ -58,7 +71,7 @@ class RxBus {
      * Returns an [Observable] of specific [Relay]
      *
      * @param key key to get specific [Relay].
-     * If null will be passed - general [Relay] will be used
+     * Default key is [KEYS.GENERAL].
      */
     @JvmOverloads
     fun observable(key: Any = KEYS.GENERAL): Observable<Any> {
@@ -73,10 +86,23 @@ class RxBus {
     }
 
     /**
-     * Enum set of default keys for RxBus.
+     * Enum set of default keys for RxBus. Every key associated with specific [Relay]
+     * - [GENERAL] - provides access to general [PublishRelay] which used as events bus
+     * - [SINGLE] - provides access to [SingleSubscriberRelay] which used as events bus.
+     * The main feature of this relay is that it always provides communication with only one subscriber.
+     * Any new subscriber will automatically overrides previous
      */
     enum class KEYS {
-        GENERAL, SINGLE
+        /**
+         * Provides access to general [PublishRelay] which used as events bus
+         */
+        GENERAL,
+        /**
+         * Provides access to [SingleSubscriberRelay] which used as events bus.
+         * The main feature of this relay is that it always provides communication with only one subscriber.
+         * Any new subscriber will automatically overrides previous
+         */
+        SINGLE
     }
 }
 
