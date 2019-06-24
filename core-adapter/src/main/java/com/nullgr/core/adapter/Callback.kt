5f085620557eb3de0c4e831d4aab36ade578c8ac
v.chernyshov <v.chernyshov@nullgr.com>
2019-06-24 09:01:45 +0300

    [#71] Make Callback public to reusable

--- core-adapter/src/main/java/com/nullgr/core/adapter/Callback.kt
+++ core-adapter/src/main/java/com/nullgr/core/adapter/Callback.kt
@@ -6,21 +6,23 @@ import com.nullgr.core.adapter.items.ListItem
 /**
  * Realization of [DiffUtil.Callback] that works with [ListItem].
  */
-internal class Callback(private val before: List<ListItem>,
-                        private val after: List<ListItem>) : DiffUtil.Callback() {
+class Callback(
+    private val before: List<ListItem>,
+    private val after: List<ListItem>
+) : DiffUtil.Callback() {
 
     override fun getOldListSize(): Int = before.size
 
     override fun getNewListSize(): Int = after.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-            before[oldItemPosition].areItemsTheSame(after[newItemPosition])
+        before[oldItemPosition].areItemsTheSame(after[newItemPosition])
 
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-            before[oldItemPosition].areContentsTheSame(after[newItemPosition])
+        before[oldItemPosition].areContentsTheSame(after[newItemPosition])
 
     override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? =
-            before[oldItemPosition].getChangePayload(after[newItemPosition])
+        before[oldItemPosition].getChangePayload(after[newItemPosition])
 
 }
