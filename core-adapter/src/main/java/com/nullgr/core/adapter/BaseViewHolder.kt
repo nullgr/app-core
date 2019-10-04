4ac5193a947ec563f7fb33769a283984a79603ed
AlexSyt <asytnyk0@gmail.com>
2019-10-04 11:35:57 +0300

    Fix formatting in BaseViewHolder

--- core-adapter/src/main/java/com/nullgr/core/adapter/BaseViewHolder.kt
+++ core-adapter/src/main/java/com/nullgr/core/adapter/BaseViewHolder.kt
@@ -1,13 +1,15 @@
 package com.nullgr.core.adapter
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Base ViewHolder for all [AdapterDelegate].
  *
  * @author a.komarovskyi
  */
-open class BaseViewHolder(parent: ViewGroup, layoutId: Int)
-    : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
+open class BaseViewHolder(
+    parent: ViewGroup,
+    layoutId: Int
+) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
